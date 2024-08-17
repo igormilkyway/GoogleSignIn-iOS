7a260674217e788e941558c28a0f68ded850d9e7
Igor Sorokin <igormilkyway1@gmail.com>
2024-08-17 03:47:19 +0300

    Remove include_granted_scopes

--- GoogleSignIn/Sources/GIDSignIn.m
+++ GoogleSignIn/Sources/GIDSignIn.m
@@ -722,7 +722,7 @@ static NSString *const kConfigOpenIDRealmKey = @"GIDOpenIDRealm";
 
   NSMutableDictionary<NSString *, NSString *> *additionalParameters =
       [[NSMutableDictionary alloc] init];
-  additionalParameters[kIncludeGrantedScopesParameter] = @"false";
+//  additionalParameters[kIncludeGrantedScopesParameter] = @"false";
   if (options.configuration.serverClientID) {
     additionalParameters[kAudienceParameter] = options.configuration.serverClientID;
   }
